87fe12ca9cfbea5744c30546308898bf7199c073
Boris Batteux <boris.batteux@eshard.com>
2021-02-25 16:35:01 +0100

    Change default value to detect dispatcher when at least 3 exit blocks are presents

--- d810/optimizers/flow/flattening/unflattener.py
+++ d810/optimizers/flow/flattening/unflattener.py
@@ -104,7 +104,7 @@ class OllvmDispatcherInfo(GenericDispatcherInfo):
 class OllvmDispatcherCollector(GenericDispatcherCollector):
     DISPATCHER_CLASS = OllvmDispatcherInfo
     DEFAULT_DISPATCHER_MIN_INTERNAL_BLOCK = 2
-    DEFAULT_DISPATCHER_MIN_EXIT_BLOCK = 2
+    DEFAULT_DISPATCHER_MIN_EXIT_BLOCK = 3
     DEFAULT_DISPATCHER_MIN_COMPARISON_VALUE = 2
 
 
